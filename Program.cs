96863d8ee79a1e075ca5f258839a246dab02adb5
Brett Andrews <brettandrews3@gmail.com>
2020-04-01 09:29:47 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotNetConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Just wakin' up in the mornin' Gotta thank god I don't know But today seems kinda odd No barking from the dog No smog And momma cooked the breakfast with no hog");
+        }
+    }
+}
